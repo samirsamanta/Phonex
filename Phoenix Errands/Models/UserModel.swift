f827dd559df37548850451bf1a622485fd370a9c
Asif <asif.dafadar@shyamfuture.com>
2019-08-27 12:17:29 +0530

    add userModel Class

---
+++ Phoenix Errands/Models/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  Phoenix Errands
+//
+//  Created by Raghav Beriwala on 27/08/19.
+//  Copyright © 2019 Shyam Future Tech. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+}
